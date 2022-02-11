c084f0ce880814f1f6da222fe89a1b4b6e1e649b
blanc <blancobielsa@gmail.com>
2022-02-11 11:53:17 +0100

    listas terminado y probado

--- TPP/Practicas/Practica1/lab1_listas/P1_listas/Clases/List.cs
+++ TPP/Practicas/Practica1/lab1_listas/P1_listas/Clases/List.cs
@@ -136,7 +136,7 @@ namespace Clases
         public override string ToString() { 
 
             StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("Length: {0} \n[", _length);
+            sb.AppendFormat("[", _length);
             Node current = _head;
             while (current != null) {
                 sb.Append(current.GetValue);
--- TPP/Practicas/Practica1/lab1_listas/P1_listas/ListTests/UnitTest1.cs
+++ TPP/Practicas/Practica1/lab1_listas/P1_listas/ListTests/UnitTest1.cs
@@ -7,15 +7,25 @@ namespace ListTests
     [TestClass]
     public class UnitTest1
     {
+        private People p1;
+        private People p2;
+        private People p3;
+        private People p4;
+        private People p5;
+
+        [TestInitialize]
+        public void InitializaPeople() {
+            p1 = new People("Juan", "Pérez", "5646453");
+            p2 = new People("Lucas", "Pérez", "562345453");
+            p3 = new People("Edu", "Pérez", "12342345");
+            p4 = new People("Carmen", "Pérez", "12343");
+            p5 = new People("Clara", "Díaz", "1324312");
+        }
+
         [TestMethod]
         public void TestAdd()
         {
             List list = new List();
-            People p1 = new People("Juan","Pérez","5646453");
-            People p2 = new People("Lucas", "Pérez", "562345453");
-            People p3 = new People("Edu", "Pérez", "12342345");
-            People p4 = new People("Carmen", "Pérez", "12343");
-            People p5 = new People("Clara", "Díaz", "1324312");
             list.Add(p1);
             list.Add(p2);
             list.Add(p3);
@@ -26,31 +36,53 @@ namespace ListTests
             Assert.AreEqual(p3, list.GetElement(2));
             Assert.AreEqual(p4, list.GetElement(3));
             Assert.AreEqual(p5, list.GetElement(4));
-
-
-
         }
 
         [TestMethod]
         public void TestRemove()
         {
             List list = new List();
-            People p1 = new People("Juan", "Pérez", "5646453");
-            People p2 = new People("Lucas", "Pérez", "562345453");
-            People p3 = new People("Edu", "Pérez", "12342345");
-            People p4 = new People("Carmen", "Pérez", "12343");
-            People p5 = new People("Clara", "Díaz", "1324312");
             list.Add(p1);
             list.Add(p2);
             list.Add(p3);
             list.Add(p4);
             list.Add(p5);
-            list.Remove(p2);
+            Assert.AreEqual(true, list.Remove(p2));
             Assert.AreEqual(4, list.Size());
             Assert.IsFalse(list.Contains(p2));
-            list.Remove(p5);
+            Assert.AreEqual(true, list.Remove(p5));
             Assert.AreEqual(3, list.Size());
             Assert.IsFalse(list.Contains(p5));
+            Assert.AreEqual(false, list.Remove(32));//se elimina algo que no existe
         }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            List list = new List();
+            Assert.AreEqual("[]", list.ToString());//si no hay ningun elemento
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+            list.Add(p5);
+            Assert.AreEqual("[Juan Pérez, with 5646453 ID number Lucas Pérez, " +
+                "with 562345453 ID number Edu Pérez, with 12342345 ID number Carmen " +
+                "Pérez, with 12343 ID number Clara Díaz, with 1324312 ID number ]", list.ToString());
+        }
+
+        [TestMethod]
+        public void GetElementTest() {
+            List list = new List();
+            Assert.IsNull(list.GetElement(3));//si no hay ningún elemento
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+            list.Add(p5);
+            Assert.AreEqual(p4, list.GetElement(3));
+            Assert.IsNull(list.GetElement(273645));
+        }
+
     }
 }
