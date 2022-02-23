3b385eb95392b39759dcc977756f77dc44330378
blanc <blancobielsa@gmail.com>
2022-02-23 12:14:45 +0100

    TPP tarea casi terminada

---
+++ TPP/Practicas/Practica 4/lab4_listas/Clases/ListEnumerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class ListEnumerator<T> : IEnumerator<T>
+    {
+        int index;//la posición del término
+        T actual;//actual term
+        int maxLength;//longitud máxima del enumerador
+
+        public ListEnumerator(int length)
+        {
+            maxLength = length;
+            Reset();
+        }
+
+        T IEnumerator<T>.Current
+        {
+            get { return actual; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return actual; }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return (index < maxLength);
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+    }
+}
