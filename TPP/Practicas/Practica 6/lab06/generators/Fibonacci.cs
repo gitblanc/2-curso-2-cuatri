9f3c1f7a22439b14f9da6306cc9d4a3bd21c5482
blanc <blancobielsa@gmail.com>
2022-03-10 13:22:15 +0100

    Terminado TPP y faltan graficas algoritmia

--- TPP/Practicas/Practica 6/lab06/generators/Fibonacci.cs
+++ TPP/Practicas/Practica 6/lab06/generators/Fibonacci.cs
@@ -21,13 +21,27 @@ namespace TPP.Laboratory.Functional.Lab06
             }
         }
 
-        static public IEnumerable<int> FibonacciLazy(int source, int target)
+        internal static int LimitedFibonacci(int n)
         {
-            return InfiniteFibonacci().Skip(source - 1).Take(target - source + 1);//saltas los sorce primeros y te quedas con los siguientes
+            return n <= 2 ? 1 : LimitedFibonacci(n - 2) + LimitedFibonacci(n - 1);
+        }
+
+        static internal IEnumerable<int> FibonacciLazy(int source, int target)
+        {
+            return InfiniteFibonacci().Skip(source - 1).Take(target - source + 1);//saltas los source primeros y te quedas con los siguientes
         }
 
         //PARA CASA:
         //EagerFibonacci
+
+        static internal IEnumerable<int> FibonacciEager(int source, int target)
+        {
+            IList<int> result = new List<int>();
+            for (int i = 0; i <= target; i++) {
+                result.Add(LimitedFibonacci(i));
+            }
+            return result.Skip(source);
+        }
 
     }
 
--- TPP/Practicas/Practica 6/lab06/generators/Program.cs
+++ TPP/Practicas/Practica 6/lab06/generators/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace TPP.Laboratory.Functional.Lab06 {
 
     class Program {
         static void Main() {
+            Stopwatch stopWatch = new Stopwatch();
             int i = 0;
             foreach (int value in Fibonacci.InfiniteFibonacci()) {
                 Console.Write(value + " ");
@@ -20,11 +23,31 @@ namespace TPP.Laboratory.Functional.Lab06 {
             }
             Console.WriteLine();
 
-            foreach (int value in Fibonacci.FibonacciLazy(10,20))
+            //-----------Lazy-----------
+            stopWatch.Start(); 
+            foreach (int value in Fibonacci.FibonacciLazy(10,1000))//forma lazy
             {
                 Console.Write(value + " ");
             }
             Console.WriteLine();
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Lazy => " + elapsedTime);
+
+            //-----------Eager-----------
+            stopWatch.Start();
+            foreach (int value in Fibonacci.FibonacciEager(10, 45))//forma eager
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            elapsedTime = string.Format("{0:00}:{1:00}:{2:00}:{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            Console.WriteLine("Eager => " + elapsedTime);
         }
     }
 }
