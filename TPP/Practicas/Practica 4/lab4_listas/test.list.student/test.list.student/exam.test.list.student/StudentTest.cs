d9210f9c15e7f0a49f5d35f3991ee3c392a308f9
blanc <blancobielsa@gmail.com>
2022-03-17 00:29:56 +0100

    Algoritmia Tromino finalizado

---
+++ TPP/Practicas/Practica 4/lab4_listas/test.list.student/test.list.student/exam.test.list.student/StudentTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TPP.Laboratory.Exam.Test.List;
+
+namespace TPP.Laboratory.Exam.Test.Student
+{
+    [TestClass]
+    public class StudentTest : TestList
+    {
+        public override dynamic GetList<T>()
+        {
+            //You must return an instance of your List
+            throw new NotImplementedException("'StudentTest.GetList' must return an instance of your generic list");
+        }
+    }
+}
