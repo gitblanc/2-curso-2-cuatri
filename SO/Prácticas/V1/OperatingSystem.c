904ac379bdcdd5c2557b8f44a21e362aab66e7ca
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-03-16 16:36:16 +0100

    SO corregido

--- SO/Prácticas/V1/OperatingSystem.c
+++ SO/Prácticas/V1/OperatingSystem.c
@@ -32,66 +32,70 @@ PCB processTable[PROCESSTABLEMAXSIZE];
 int OS_address_base = PROCESSTABLEMAXSIZE * MAINMEMORYSECTIONSIZE;
 
 // Identifier of the current executing process
-int executingProcessID=NOPROCESS;
+int executingProcessID = NOPROCESS;
 
 // Identifier of the System Idle Process
 int sipID;
 
 // Initial PID for assignation
-int initialPID=0;
+int initialPID = 0;
 
 // Begin indes for daemons in programList
-int baseDaemonsInProgramList; 
+int baseDaemonsInProgramList;
 
 // Array that contains the identifiers of the READY processes
 heapItem readyToRunQueue[PROCESSTABLEMAXSIZE];
-int numberOfReadyToRunProcesses=0;
+int numberOfReadyToRunProcesses = 0;
 
 // Variable containing the number of not terminated user processes
-int numberOfNotTerminatedUserProcesses=0;
+int numberOfNotTerminatedUserProcesses = 0;
 
-char DAEMONS_PROGRAMS_FILE[MAXIMUMLENGTH]="teachersDaemons";
+char DAEMONS_PROGRAMS_FILE[MAXIMUMLENGTH] = "teachersDaemons";
 
 // Initial set of tasks of the OS
-void OperatingSystem_Initialize(int daemonsIndex) {
-	
+void OperatingSystem_Initialize(int daemonsIndex)
+{
+
 	int i, selectedProcess;
 	FILE *programFile; // For load Operating System Code
-	programFile=fopen("OperatingSystemCode", "r");
-	if (programFile==NULL){
+	programFile = fopen("OperatingSystemCode", "r");
+	if (programFile == NULL)
+	{
 		// Show red message "FATAL ERROR: Missing Operating System!\n"
-		ComputerSystem_DebugMessage(99,SHUTDOWN,"FATAL ERROR: Missing Operating System!\n");
-		exit(1);		
+		ComputerSystem_DebugMessage(99, SHUTDOWN, "FATAL ERROR: Missing Operating System!\n");
+		exit(1);
 	}
 
 	// Obtain the memory requirements of the program
-	int processSize=OperatingSystem_ObtainProgramSize(programFile);
+	int processSize = OperatingSystem_ObtainProgramSize(programFile);
 
 	// Load Operating System Code
 	OperatingSystem_LoadProgram(programFile, OS_address_base, processSize);
-	
+
 	// Process table initialization (all entries are free)
-	for (i=0; i<PROCESSTABLEMAXSIZE;i++){
-		processTable[i].busy=0;
+	for (i = 0; i < PROCESSTABLEMAXSIZE; i++)
+	{
+		processTable[i].busy = 0;
 	}
 	// Initialization of the interrupt vector table of the processor
-	Processor_InitializeInterruptVectorTable(OS_address_base+2);
-		
+	Processor_InitializeInterruptVectorTable(OS_address_base + 2);
+
 	// Include in program list  all system daemon processes
 	OperatingSystem_PrepareDaemons(daemonsIndex);
-	
+
 	// Create all user processes from the information given in the command line
 	OperatingSystem_LongTermScheduler();
-	
-	if (strcmp(programList[processTable[sipID].programListIndex]->executableName,"SystemIdleProcess")) {
+
+	if (strcmp(programList[processTable[sipID].programListIndex]->executableName, "SystemIdleProcess"))
+	{
 		// Show red message "FATAL ERROR: Missing SIP program!\n"
-		ComputerSystem_DebugMessage(99,SHUTDOWN,"FATAL ERROR: Missing SIP program!\n");
-		exit(1);		
+		ComputerSystem_DebugMessage(99, SHUTDOWN, "FATAL ERROR: Missing SIP program!\n");
+		exit(1);
 	}
 
 	// At least, one user process has been created
 	// Select the first process that is going to use the processor
-	selectedProcess=OperatingSystem_ShortTermScheduler();
+	selectedProcess = OperatingSystem_ShortTermScheduler();
 
 	// Assign the processor to the selected process
 	OperatingSystem_Dispatch(selectedProcess);
@@ -101,34 +105,36 @@ void OperatingSystem_Initialize(int daemonsIndex) {
 }
 
 // The LTS is responsible of the admission of new processes in the system.
-// Initially, it creates a process from each program specified in the 
+// Initially, it creates a process from each program specified in the
 // 			command line and daemons programs
-int OperatingSystem_LongTermScheduler() {
-  
-	int PID, i,
-		numberOfSuccessfullyCreatedProcesses=0;
-	for (i=0; programList[i]!=NULL && i<PROGRAMSMAXNUMBER ; i++) {
-		PID=OperatingSystem_CreateProcess(i);
-		//EJ 4 V1
-		if(PID == NOFREEENTRY){
-			ComputerSystem_DebugMessage(103, ERROR, programList[i]->executableName);
-		}	
-		//EJ5 V1
-		if(PID == PROGRAMDOESNOTEXIST){
-			ComputerSystem_DebugMessage(104, ERROR, programList[i]->executableName, "it does not exists");
-		}
+int OperatingSystem_LongTermScheduler()
+{
 
-		if(PID == PROGRAMNOTVALID){
+	int PID, i,
+		numberOfSuccessfullyCreatedProcesses = 0;
+	for (i = 0; programList[i] != NULL && i < PROGRAMSMAXNUMBER; i++)
+	{
+		PID = OperatingSystem_CreateProcess(i);
+		switch (PID)
+		{
+		// EJ 4 V1
+		case NOFREEENTRY:
+			ComputerSystem_DebugMessage(103, ERROR, programList[i]->executableName);
+
+		// EJ5 V1
+		case PROGRAMDOESNOTEXIST:
+			ComputerSystem_DebugMessage(104, ERROR, programList[i]->executableName, "it does not exists");
+
+		case PROGRAMNOTVALID:
 			ComputerSystem_DebugMessage(104, ERROR, programList[i]->executableName, "invalid priority or size");
-		}
-		//EJ 6 V1
-		if(PID == TOOBIGPROCESS){
+
+		// EJ 6 V1
+		case TOOBIGPROCESS:
 			ComputerSystem_DebugMessage(105, ERROR, programList[i]->executableName);
-		}
-		
-		else{
+
+		default:
 			numberOfSuccessfullyCreatedProcesses++;
-			if (programList[i]->type==USERPROGRAM) 
+			if (programList[i]->type == USERPROGRAM)
 				numberOfNotTerminatedUserProcesses++;
 			// Move process to the ready state
 			OperatingSystem_MoveToTheREADYState(PID);
@@ -139,248 +145,260 @@ int OperatingSystem_LongTermScheduler() {
 	return numberOfSuccessfullyCreatedProcesses;
 }
 
-
 // This function creates a process from an executable program
-int OperatingSystem_CreateProcess(int indexOfExecutableProgram) {
-  
+int OperatingSystem_CreateProcess(int indexOfExecutableProgram)
+{
+
 	int PID;
 	int processSize;
 	int loadingPhysicalAddress;
 	int priority;
 	FILE *programFile;
-	PROGRAMS_DATA *executableProgram=programList[indexOfExecutableProgram];
+	PROGRAMS_DATA *executableProgram = programList[indexOfExecutableProgram];
 
 	// Obtain a process ID
-	PID=OperatingSystem_ObtainAnEntryInTheProcessTable();
-	//EJ 4 V1-x1
-	if(PID < 0){
+	PID = OperatingSystem_ObtainAnEntryInTheProcessTable();
+	// EJ 4 V1-x1
+	if (PID < 0)
+	{
 		return NOFREEENTRY;
 	}
 	// Check if programFile exists
-	
-	//EJ 5 V1
-	programFile=fopen(executableProgram->executableName, "r");
-	if(programFile == NULL){
+
+	// EJ 5 V1
+	programFile = fopen(executableProgram->executableName, "r");
+	if (programFile == NULL)
+	{
 		return PROGRAMDOESNOTEXIST;
 	}
 
 	// Obtain the memory requirements of the program
-	//EJ 5 V1
-	processSize=OperatingSystem_ObtainProgramSize(programFile);	
-	if(processSize == 0){
+	// EJ 5 V1
+	processSize = OperatingSystem_ObtainProgramSize(programFile);
+	if (processSize == 0)
+	{
 		return PROGRAMNOTVALID;
 	}
 
 	// Obtain the priority for the process
-	priority=OperatingSystem_ObtainPriority(programFile);
-	
+	priority = OperatingSystem_ObtainPriority(programFile);
+	if (PID == PROGRAMNOTVALID)
+	{
+		return PROGRAMNOTVALID;
+	}
 	// Obtain enough memory space
-	//EJ 6 V1
- 	loadingPhysicalAddress=OperatingSystem_ObtainMainMemory(processSize, PID);
-	if(loadingPhysicalAddress == TOOBIGPROCESS){
+	// EJ 6 V1
+	loadingPhysicalAddress = OperatingSystem_ObtainMainMemory(processSize, PID);
+	if (loadingPhysicalAddress == TOOBIGPROCESS)
+	{
 		return TOOBIGPROCESS;
 	}
 
 	// Load program in the allocated memory
 	OperatingSystem_LoadProgram(programFile, loadingPhysicalAddress, processSize);
-	
+
 	// PCB initialization
 	OperatingSystem_PCBInitialization(PID, loadingPhysicalAddress, processSize, priority, indexOfExecutableProgram);
-	
+
 	// Show message "Process [PID] created from program [executableName]\n"
-	ComputerSystem_DebugMessage(70,INIT,PID,executableProgram->executableName);
-	
+	ComputerSystem_DebugMessage(70, INIT, PID, executableProgram->executableName);
+
 	return PID;
 }
 
-
 // Main memory is assigned in chunks. All chunks are the same size. A process
 // always obtains the chunk whose position in memory is equal to the processor identifier
-int OperatingSystem_ObtainMainMemory(int processSize, int PID) {
+int OperatingSystem_ObtainMainMemory(int processSize, int PID)
+{
 
- 	if (processSize>MAINMEMORYSECTIONSIZE)
+	if (processSize > MAINMEMORYSECTIONSIZE)
 		return TOOBIGPROCESS;
-	
- 	return PID*MAINMEMORYSECTIONSIZE;
-}
 
+	return PID * MAINMEMORYSECTIONSIZE;
+}
 
 // Assign initial values to all fields inside the PCB
-void OperatingSystem_PCBInitialization(int PID, int initialPhysicalAddress, int processSize, int priority, int processPLIndex) {
+void OperatingSystem_PCBInitialization(int PID, int initialPhysicalAddress, int processSize, int priority, int processPLIndex)
+{
 
-	processTable[PID].busy=1;
-	processTable[PID].initialPhysicalAddress=initialPhysicalAddress;
-	processTable[PID].processSize=processSize;
-	processTable[PID].state=NEW;
-	processTable[PID].priority=priority;
-	processTable[PID].programListIndex=processPLIndex;
+	processTable[PID].busy = 1;
+	processTable[PID].initialPhysicalAddress = initialPhysicalAddress;
+	processTable[PID].processSize = processSize;
+	processTable[PID].state = NEW;
+	processTable[PID].priority = priority;
+	processTable[PID].programListIndex = processPLIndex;
 	// Daemons run in protected mode and MMU use real address
-	if (programList[processPLIndex]->type == DAEMONPROGRAM) {
-		processTable[PID].copyOfPCRegister=initialPhysicalAddress;
-		processTable[PID].copyOfPSWRegister= ((unsigned int) 1) << EXECUTION_MODE_BIT;
-	} 
-	else {
-		processTable[PID].copyOfPCRegister=0;
-		processTable[PID].copyOfPSWRegister=0;
+	if (programList[processPLIndex]->type == DAEMONPROGRAM)
+	{
+		processTable[PID].copyOfPCRegister = initialPhysicalAddress;
+		processTable[PID].copyOfPSWRegister = ((unsigned int)1) << EXECUTION_MODE_BIT;
 	}
-
+	else
+	{
+		processTable[PID].copyOfPCRegister = 0;
+		processTable[PID].copyOfPSWRegister = 0;
+	}
 }
-
 
 // Move a process to the READY state: it will be inserted, depending on its priority, in
 // a queue of identifiers of READY processes
-void OperatingSystem_MoveToTheREADYState(int PID) {
-	
-	if (Heap_add(PID, readyToRunQueue,QUEUE_PRIORITY ,&numberOfReadyToRunProcesses ,PROCESSTABLEMAXSIZE)>=0) {
-		processTable[PID].state=READY;
-	} 
-}
+void OperatingSystem_MoveToTheREADYState(int PID)
+{
 
+	if (Heap_add(PID, readyToRunQueue, QUEUE_PRIORITY, &numberOfReadyToRunProcesses, PROCESSTABLEMAXSIZE) >= 0)
+	{
+		processTable[PID].state = READY;
+	}
+}
 
 // The STS is responsible of deciding which process to execute when specific events occur.
 // It uses processes priorities to make the decission. Given that the READY queue is ordered
 // depending on processes priority, the STS just selects the process in front of the READY queue
-int OperatingSystem_ShortTermScheduler() {
-	
+int OperatingSystem_ShortTermScheduler()
+{
+
 	int selectedProcess;
 
-	selectedProcess=OperatingSystem_ExtractFromReadyToRun();
-	
+	selectedProcess = OperatingSystem_ExtractFromReadyToRun();
+
 	return selectedProcess;
 }
 
-
 // Return PID of more priority process in the READY queue
-int OperatingSystem_ExtractFromReadyToRun() {
-  
-	int selectedProcess=NOPROCESS;
+int OperatingSystem_ExtractFromReadyToRun()
+{
 
-	selectedProcess=Heap_poll(readyToRunQueue,QUEUE_PRIORITY ,&numberOfReadyToRunProcesses);
-	
+	int selectedProcess = NOPROCESS;
+
+	selectedProcess = Heap_poll(readyToRunQueue, QUEUE_PRIORITY, &numberOfReadyToRunProcesses);
+
 	// Return most priority process or NOPROCESS if empty queue
-	return selectedProcess; 
+	return selectedProcess;
 }
 
-
 // Function that assigns the processor to a process
-void OperatingSystem_Dispatch(int PID) {
+void OperatingSystem_Dispatch(int PID)
+{
 
 	// The process identified by PID becomes the current executing process
-	executingProcessID=PID;
+	executingProcessID = PID;
 	// Change the process' state
-	processTable[PID].state=EXECUTING;
+	processTable[PID].state = EXECUTING;
 	// Modify hardware registers with appropriate values for the process identified by PID
 	OperatingSystem_RestoreContext(PID);
 }
 
-
 // Modify hardware registers with appropriate values for the process identified by PID
-void OperatingSystem_RestoreContext(int PID) {
-  
+void OperatingSystem_RestoreContext(int PID)
+{
+
 	// New values for the CPU registers are obtained from the PCB
-	Processor_CopyInSystemStack(MAINMEMORYSIZE-1,processTable[PID].copyOfPCRegister);
-	Processor_CopyInSystemStack(MAINMEMORYSIZE-2,processTable[PID].copyOfPSWRegister);
-	
+	Processor_CopyInSystemStack(MAINMEMORYSIZE - 1, processTable[PID].copyOfPCRegister);
+	Processor_CopyInSystemStack(MAINMEMORYSIZE - 2, processTable[PID].copyOfPSWRegister);
+
 	// Same thing for the MMU registers
 	MMU_SetBase(processTable[PID].initialPhysicalAddress);
 	MMU_SetLimit(processTable[PID].processSize);
 }
 
-
-// Function invoked when the executing process leaves the CPU 
-void OperatingSystem_PreemptRunningProcess() {
+// Function invoked when the executing process leaves the CPU
+void OperatingSystem_PreemptRunningProcess()
+{
 
 	// Save in the process' PCB essential values stored in hardware registers and the system stack
 	OperatingSystem_SaveContext(executingProcessID);
 	// Change the process' state
 	OperatingSystem_MoveToTheREADYState(executingProcessID);
 	// The processor is not assigned until the OS selects another process
-	executingProcessID=NOPROCESS;
+	executingProcessID = NOPROCESS;
 }
-
 
 // Save in the process' PCB essential values stored in hardware registers and the system stack
-void OperatingSystem_SaveContext(int PID) {
-	
+void OperatingSystem_SaveContext(int PID)
+{
+
 	// Load PC saved for interrupt manager
-	processTable[PID].copyOfPCRegister=Processor_CopyFromSystemStack(MAINMEMORYSIZE-1);
-	
+	processTable[PID].copyOfPCRegister = Processor_CopyFromSystemStack(MAINMEMORYSIZE - 1);
+
 	// Load PSW saved for interrupt manager
-	processTable[PID].copyOfPSWRegister=Processor_CopyFromSystemStack(MAINMEMORYSIZE-2);
-	
+	processTable[PID].copyOfPSWRegister = Processor_CopyFromSystemStack(MAINMEMORYSIZE - 2);
 }
 
-
 // Exception management routine
-void OperatingSystem_HandleException() {
-  
+void OperatingSystem_HandleException()
+{
+
 	// Show message "Process [executingProcessID] has generated an exception and is terminating\n"
-	ComputerSystem_DebugMessage(71,SYSPROC,executingProcessID,programList[processTable[executingProcessID].programListIndex]->executableName);
-	
+	ComputerSystem_DebugMessage(71, SYSPROC, executingProcessID, programList[processTable[executingProcessID].programListIndex]->executableName);
+
 	OperatingSystem_TerminateProcess();
 }
 
-
 // All tasks regarding the removal of the process
-void OperatingSystem_TerminateProcess() {
-  
+void OperatingSystem_TerminateProcess()
+{
+
 	int selectedProcess;
-  	
-	processTable[executingProcessID].state=EXIT;
-	
-	if (programList[processTable[executingProcessID].programListIndex]->type==USERPROGRAM) 
+
+	processTable[executingProcessID].state = EXIT;
+
+	if (programList[processTable[executingProcessID].programListIndex]->type == USERPROGRAM)
 		// One more user process that has terminated
 		numberOfNotTerminatedUserProcesses--;
-	
-	if (numberOfNotTerminatedUserProcesses==0) {
-		if (executingProcessID==sipID) {
+
+	if (numberOfNotTerminatedUserProcesses == 0)
+	{
+		if (executingProcessID == sipID)
+		{
 			// finishing sipID, change PC to address of OS HALT instruction
 			OperatingSystem_TerminatingSIP();
-			ComputerSystem_DebugMessage(99,SHUTDOWN,"The system will shut down now...\n");
+			ComputerSystem_DebugMessage(99, SHUTDOWN, "The system will shut down now...\n");
 			return; // Don't dispatch any process
 		}
 		// Simulation must finish, telling sipID to finish
 		OperatingSystem_ReadyToShutdown();
 	}
 	// Select the next process to execute (sipID if no more user processes)
-	selectedProcess=OperatingSystem_ShortTermScheduler();
+	selectedProcess = OperatingSystem_ShortTermScheduler();
 
 	// Assign the processor to that process
 	OperatingSystem_Dispatch(selectedProcess);
 }
 
 // System call management routine
-void OperatingSystem_HandleSystemCall() {
-  
+void OperatingSystem_HandleSystemCall()
+{
+
 	int systemCallID;
 
 	// Register A contains the identifier of the issued system call
-	systemCallID=Processor_GetRegisterA();
-	
-	switch (systemCallID) {
-		case SYSCALL_PRINTEXECPID:
-			// Show message: "Process [executingProcessID] has the processor assigned\n"
-			ComputerSystem_DebugMessage(72,SYSPROC,executingProcessID,programList[processTable[executingProcessID].programListIndex]->executableName);
-			break;
+	systemCallID = Processor_GetRegisterA();
 
-		case SYSCALL_END:
-			// Show message: "Process [executingProcessID] has requested to terminate\n"
-			ComputerSystem_DebugMessage(73,SYSPROC,executingProcessID,programList[processTable[executingProcessID].programListIndex]->executableName);
-			OperatingSystem_TerminateProcess();
-			break;
+	switch (systemCallID)
+	{
+	case SYSCALL_PRINTEXECPID:
+		// Show message: "Process [executingProcessID] has the processor assigned\n"
+		ComputerSystem_DebugMessage(72, SYSPROC, executingProcessID, programList[processTable[executingProcessID].programListIndex]->executableName);
+		break;
+
+	case SYSCALL_END:
+		// Show message: "Process [executingProcessID] has requested to terminate\n"
+		ComputerSystem_DebugMessage(73, SYSPROC, executingProcessID, programList[processTable[executingProcessID].programListIndex]->executableName);
+		OperatingSystem_TerminateProcess();
+		break;
 	}
 }
-	
+
 //	Implement interrupt logic calling appropriate interrupt handle
-void OperatingSystem_InterruptLogic(int entryPoint){
-	switch (entryPoint){
-		case SYSCALL_BIT: // SYSCALL_BIT=2
-			OperatingSystem_HandleSystemCall();
-			break;
-		case EXCEPTION_BIT: // EXCEPTION_BIT=6
-			OperatingSystem_HandleException();
-			break;
+void OperatingSystem_InterruptLogic(int entryPoint)
+{
+	switch (entryPoint)
+	{
+	case SYSCALL_BIT: // SYSCALL_BIT=2
+		OperatingSystem_HandleSystemCall();
+		break;
+	case EXCEPTION_BIT: // EXCEPTION_BIT=6
+		OperatingSystem_HandleException();
+		break;
 	}
-
 }
-
