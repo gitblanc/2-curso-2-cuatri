a301910d4645bec6a0b0f631b021ed2f4ea4449d
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-03-23 19:42:03 +0100

    cn

---
+++ CN/Prácticas/P6/main.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Parámetros de entrada: k, índice del polinomio fundamental; x, nodos del problema;
+# z, polinomio a calcular
+
+# devuelve el valor k-esimo polinomio fundamental
+def lagrange_fund(k, x, z):
+    x0 = x[0]
+    xk = x[k]
+    if k != 0:
+        lk = (z - x0) / (xk - x0)
+    else:
+        lk = 1
+    for i in range(1, len(x)):
+        if i == k:
+            lk = lk
+        else:
+            lk = lk * (z - x[i]) / (xk - x[i])
+    return lk
+
+
+def lagrange_fundamental(k, x, z):
+    n = np.zeros(len(z))
+    for j in range(len(z)):
+        n[j] = lagrange_fund(k, x, z[j])
+    return n
+
+
+x = np.array([-1., 0, 2, 3, 5])
+k = 2
+z = np.array([1.3, 2.1, 3.2])
+yz = lagrange_fundamental(k, x, z)
+print(yz)
+print()
+print(np.eye(len(x)))
+y = np.array([1., 3, 4, 3, 1])
+xp = np.linspace(min(x), max(x))
+p = np.polyfit(x, y, len(x) - 1)
+
+plt.figure()
+plt.plot(lagrange_fundamental(1, x, z))
+plt.plot(x, 0 * x, 'k')
+plt.title('L1')
+plt.show()
+
+plt.figure()
+plt.plot(lagrange_fundamental(2, x, z))
+plt.plot(x, 0 * x, 'k')
+plt.title('L2')
+plt.show()
+
+plt.figure()
+plt.plot(lagrange_fundamental(3, x, z))
+plt.plot(x, 0 * x, 'k')
+plt.title('L3')
+plt.show()
+
+plt.figure()
+plt.plot(lagrange_fundamental(4, x, z))
+plt.plot(x, 0 * x, 'k')
+plt.title('L4')
+plt.show()
